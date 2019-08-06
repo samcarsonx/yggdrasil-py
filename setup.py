20d8afa864ae09bbe25c26d396e51b0986d7c635
Sam Carson <sam.carson368@gmail.com>
2019-08-06 12:29:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'yggdrasil-py',
   packages = ['yggdrasil'],
-  version = '1.0.3',
+  version = '1.0.4',
   license='MIT',
   description = 'Python wrapper for Mojang\'s Yggdrasil authentication service.',
   long_description=long_description,
@@ -16,7 +16,7 @@ setup(
   author = 'Sam Carson',
   author_email = 'me@samcarson.xyz',
   url = 'https://github.com/samcarsonx/yggdrasil-py',
-  download_url = 'https://github.com/samcarsonx/yggdrasil-py/archive/1.0.3.tar.gz',
+  download_url = 'https://github.com/samcarsonx/yggdrasil-py/archive/1.0.4.tar.gz',
   keywords = ['mojang','yggdrasil','minecraft','scrolls','authentication'],
   install_requires = [],
   classifiers=[
