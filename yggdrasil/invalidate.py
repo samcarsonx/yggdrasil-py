e41662cd3e4842da9b14cba04193beaba03051f9
unknown <onixcontactmail@gmail.com>
2020-10-10 11:30:30 +0600

    Added parameters for using custom authentication servers

--- yggdrasil/invalidate.py
+++ yggdrasil/invalidate.py
@@ -3,16 +3,17 @@ import requests, json
 url = 'https://authserver.mojang.com'
 headers = {'Content-Type': 'application/json'}
 
-def invalidate(accessToken:str, clientToken:str):
+def invalidate(accessToken:str, clientToken:str, authServer:str = url):
     '''
     Invalidates accessTokens using a client/access token pair.
     Parameters:
       accessToken - Valid accessToken, gained from authenticate()
       clientToken - Identical to the clientToken used to get the accessToken in the first place
+      authServer - Authentication server, defaults to authserver.mojang.com
     Returns: True if success, otherwise the error
     '''
     data = json.dumps({"accessToken":accessToken, "clientToken":clientToken})
-    try: response = json.loads(requests.post(url + '/invalidate', data=data, headers=headers).text)
+    try: response = json.loads(requests.post(authServer + '/invalidate', data=data, headers=headers).text)
     except json.decoder.JSONDecodeError: return True
     if 'error' in response: raise Exception(f"{response['error']}: {response['errorMessage']}")
     return False
